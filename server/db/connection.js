858992eef53aa0efceaf0f2460cf570a17e92a79
ajit.none <129359686+lazer002@users.noreply.github.com>
2024-10-17 00:04:33 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,5 @@
 const mon = require('mongoose')
-const mongo = mon.connect(process.env.DB)
+const mongo = mon.connect('mongodb+srv://admin123:U9EfIgWx937O08k6@cluster0.r23nlsu.mongodb.net/sukriti?retryWrites=true&w=majority&appName=Cluster0')
 .then((res)=>{
     console.log('connected');
 })
